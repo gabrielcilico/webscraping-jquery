6961ffda0032b8b028a6a58e26b21718f55ffb1a
gabrielcilico <gabrielcilico@gmail.com>
2020-05-21 20:28:23 -0300

    Ajustes finos

--- webScraping.js
+++ webScraping.js
@@ -1,29 +1,28 @@
 function getTitle(father) {
-    return father.find(".product-card__title").text();
+  return father.find(".product-card__title").text();
 }
 
 function getDiscount(father) {
-    return father.find(".promotion__tag-discount").text();
+  return father.find(".promotion__tag-discount").text();
 }
 
 function getValue(father) {
-    return father.find(".promotion-price").text();
+  return father.find(".promotion-price").text();
 }
 
 function getJson() {
-	let cards = $(".product-card__link");
-	let jsonCards = [];
-	
-	$.each($(".product-card"), function() {
-		let currentCard = $(this);
-		jsonCards.push({
-			title: getTitle(currentCard),
-			value: getValue(currentCard),
-			discount: getDiscount(currentCard)
-		})
-	});
-	
-	return JSON.stringify(jsonCards, "", 2)
+  let cards = $(".product-card");
+  let jsonCards = [];
+
+  cards.each((currentCard) => {
+    jsonCards.push({
+      title: getTitle(currentCard),
+      value: getValue(currentCard),
+      discount: getDiscount(currentCard),
+    });
+  });
+
+  return JSON.stringify(jsonCards, "", 2);
 }
 
 getJson();
